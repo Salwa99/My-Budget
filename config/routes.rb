eb2a96cd8b01fe2bc8439c438cf5af3e71a60a80
Salwa99 <s.ritage99@gmail.com>
2023-05-14 15:03:33 +0100

    Add authentication pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
+  root 'home#index'
+
+  resources :categories do
+    resources :expenses, only: %i[index create new]
+  end
+
+  resources :expenses, only: %i[edit update destroy]
 end
