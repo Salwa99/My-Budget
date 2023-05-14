fbdc25c0b9efd0bf1e758229bd4ff9eff511dfaa
Salwa99 <s.ritage99@gmail.com>
2023-05-14 21:24:37 +0100

    Update the category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,33 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @categories = Category.all
+    @categories = current_user.categories
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+
+    if @category.save
+      redirect_to root_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def expenses
+    @category = current_user.categories.find(params[:category_id])
+    @expenses = @category.expenses.order(date: :desc)
+    @total_amount = @expenses.sum(:amount)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
