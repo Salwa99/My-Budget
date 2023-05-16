4949a380f16dbb1158028b2361f95010654636e3
Salwa99 <s.ritage99@gmail.com>
2023-05-16 11:20:27 +0100

    Fix the expenses view

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,25 +1,33 @@
-class Categories::ExpensesController < ApplicationController
-  before_action :authenticate_user!
+module Categories
+  class ExpensesController < ApplicationController
+    before_action :authenticate_user!
 
-  def new
-    @category = current_user.categories.find(params[:category_id])
-    @expense = @category.expenses.build
-  end
-
-  def create
-    @category = current_user.categories.find(params[:category_id])
-    @expense = @category.expenses.build(expense_params)
-
-    if @expenses.save
-      redirect_to category_expenses_path(@category), notice: 'Transaction was successfully created.'
-    else
-      render :new
+    def index
+      @category = current_user.categories.find(params[:category_id])
+      @expenses = @category.expenses.order(date: :desc)
+      @total_amount = @expenses.sum(:amount)
     end
-  end
 
-  private
+    def new
+      @category = current_user.categories.find(params[:category_id])
+      @expense = @category.expenses.build
+    end
 
-  def expense_params
-    params.require(:expense).permit(:date, :description, :amount)
+    def create
+      @category = current_user.categories.find(params[:category_id])
+      @expense = @category.expenses.build(expense_params)
+
+      if @expense.save
+        redirect_to category_expenses_path(@category), notice: 'Expense was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def expense_params
+      params.require(:expense).permit(:date, :amount)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root 'home#index'
 
   authenticated :user do
     root 'categories#index', as: :authenticated_root
   end
 
-  resources :categories do
-    resources :expenses, only: %i[index create new], module: :categories
+  resources :categories, module: :categories do
+    resources :expenses
   end
-
-  resources :expenses, only: %i[edit update destroy]
 end
