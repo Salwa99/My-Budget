3b00b9d6e41e636bef591a8dd5169064b7def7d3
Salwa99 <s.ritage99@gmail.com>
2023-05-19 20:43:14 +0100

    Deploy the app

--- config/puma.rb
+++ config/puma.rb
@@ -35,9 +35,9 @@ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
-process behavior so workers use less memory.
+# process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
