64fc39e8b8c6922111e6af071ae3de9d122369b5
Salwa99 <s.ritage99@gmail.com>
2023-05-20 13:17:25 +0100

    Fix the test

--- spec/features/category_index_spec.rb
+++ spec/features/category_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Categories Index Page', type: :feature do
+RSpec.feature 'Categories Index', type: :feature do
   include Devise::Test::IntegrationHelpers
 
   let!(:user) { User.create!(name: 'Salwa', email: 'salwa@gmail.com', password: 'password') }
@@ -12,40 +12,35 @@ RSpec.feature 'Categories Index Page', type: :feature do
     visit categories_path
   end
 
-  describe 'displays the categories with their details' do
-    it 'displays the categories with their names and icons' do
-      expect(page).to have_content('Categories')
+  scenario 'displays the categories with their details' do
+    expect(page).to have_content('Categories')
+
+    within('.categories') do
       expect(page).to have_content(category1.name)
-      expect(page).to have_content(category1.icon)
+      expect(page).to have_content(category1.expenses.collect(&:amount).sum)
       expect(page).to have_content(category2.name)
-      expect(page).to have_content(category2.icon)
+      expect(page).to have_content(category2.expenses.collect(&:amount).sum)
     end
   end
 
-  describe 'displays buttons to view expenses and add a new expense for each category' do
-    it 'displays buttons to view expenses and add a new expense' do
-      within('.categories') do
-        expect(page).to have_link('View Expenses', href: category_expenses_path(category1))
-        expect(page).to have_link('View Expenses', href: category_expenses_path(category2))
-        expect(page).to have_link('Add a New Expense', href: new_category_expense_path(category1))
-        expect(page).to have_link('Add a New Expense', href: new_category_expense_path(category2))
-      end
+  scenario 'displays buttons to view expenses and add a new expense for each category' do
+    within('.categories') do
+      expect(page).to have_link('View Expenses', href: category_expenses_path(category1))
+      expect(page).to have_link('View Expenses', href: category_expenses_path(category2))
+      expect(page).to have_link('Add a New Expense', href: new_category_expense_path(category1))
+      expect(page).to have_link('Add a New Expense', href: new_category_expense_path(category2))
     end
   end
 
-  describe 'displays a button to add a new category' do
-    it 'displays a button to add a new category' do
-      expect(page).to have_link('Add a New Category', href: new_category_path)
-    end
+  scenario 'displays a button to add a new category' do
+    expect(page).to have_link('Scan', href: new_category_path)
   end
 
-  describe 'displays a message and a button to add a new category when no categories exist' do
-    it 'displays a message and a button when no categories exist' do
-      Category.destroy_all
-      visit categories_path
+  scenario 'displays a message and a button to add a new category when no categories exist' do
+    Category.destroy_all
+    visit categories_path
 
-      expect(page).to have_content('No categories found. Click the button below to add a new category.')
-      expect(page).to have_link('Add a New Category', href: new_category_path)
-    end
+    expect(page).to have_content('No categories found. Click the button below to add a new category.')
+    expect(page).to have_link('Scan', href: new_category_path)
   end
 end
